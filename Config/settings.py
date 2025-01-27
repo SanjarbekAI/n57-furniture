2d67cb224f7c2643152e6271bc16f748111d4b05
SanjarbekAI <sanjarbekwork@gmail.com>
2025-01-27 17:04:49 +0500

    register

--- Config/settings.py
+++ Config/settings.py
@@ -101,3 +101,10 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sanjarbekwork@gmail.com'
+EMAIL_HOST_PASSWORD = 'llzc ngfi szjs djeh'
